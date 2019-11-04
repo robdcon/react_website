addbba813e5272bf63aa665aff165bdfad03cb51
robdcon <robdcon@gmail.com>
2019-11-04 15:22:32 +0000

    Basic page setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from './components/Pages/Home'
+import PageWrapper from './components/PageWrapper'
+
+class App extends Component {
+  render() {
+    return (
+     <PageWrapper>
+       <Home/>
+     </PageWrapper>
+    );
+  }
+}
+
+export default App;
