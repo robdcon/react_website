b1ac17f89bbbddc1abe61ac33dd1a5bb8fe2087d
robdcon <robdcon@gmail.com>
2019-11-05 09:44:10 +0000

    Adds react-router-dom and about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Home from './components/Pages/Home'
+import About from './components/Pages/About'
 import PageWrapper from './components/PageWrapper'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-     <PageWrapper>
-       <Home/>
+
+    <Router>
+     <PageWrapper> 
+
+         <Route 
+          exact={true}
+          path='/'
+          component={Home}
+        /> 
+
+        <Route 
+          exact={true}
+          path='/about'
+          component={About}
+        /> 
+
      </PageWrapper>
+    </Router>
+
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+class Header extends Component 
+{
+    constructor(props)
+    {
+        super(props)
+    }
+    render() {
+      return (
+        <div>
+            <header className="masthead" style={{backgroudImage:this.props.image}}>
+                <div className="container">
+                <div className="intro-text">
+                    <div className="intro-lead-in">{this.props.title}</div>
+                    <div className="intro-heading text-uppercase">{this.props.subtitle}</div>
+                    {this.props.showBtn && <Link className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" to="/services">{this.props.btnText}</Link>}
+                </div>
+                </div>
+            </header>
+        </div>
+      );
+    }
+  }
+  
+  export default Header;
