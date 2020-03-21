300524c346507f4bbc754b277d3f0aa54e21761b
robdcon <robdcon@gmail.com>
2020-03-21 12:25:01 +0000

    develops dashboard

--- src/components/AdminWrapper.js
+++ src/components/AdminWrapper.js
@@ -1,20 +1,110 @@
 import React, {Component} from 'react';
-import ButtonAppBar from './AppBar';
-import Sidebar from './Sidebar';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { Drawer, ListItem, ListItemIcon, ListItemText, IconButton, Divider, AppBar, Toolbar, Typography } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import Dashboard from '@material-ui/icons/Dashboard';
+import MenuOpenIcon from '@material-ui/icons/MenuOpen';
+import Subject from '@material-ui/icons/Subject';
+import classNames from 'classnames';
+import SideBar from './Sidebar';
 
 
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+
+    toolbar: {
+        paddingRight: 24
+    },
+    drawerPaper: {
+        position:'relative',
+        whiteSpace: 'no-wrap',
+        width:drawerWidth
+    },
+    drawerPaperClosed: {
+        overflowX: 'hidden',
+        width: theme.spacing(7),
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    appBar: {
+        marginLeft:0
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth})`
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar
+    }
+});
 
 class AdminWrapper extends Component
 {
+    constructor(props){
+        super(props);
+        this.state = {
+            open:false
+        }
+    }
+
+    handleOpen = (e) => {
+        this.setState({open: true})
+    }
+
+    handleClose = (e) => {
+        this.setState({open: false})
+    }
+    
     render()
     {
+        const {classes} = this.props;
         return(
             <div id="admin-wrapper">
 
-                <Sidebar />
-                <ButtonAppBar />
+                <AppBar
+                  className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+                >
+                    <Toolbar
+                     className={classes.toolbar}
+                    >
+                        <IconButton onClick={this.handleOpen} className="menuButton">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        >
+                            Admin
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
 
-              
+                <Drawer 
+                classes={{
+                    paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClosed)}}
+                variant="permanent"
+                open={true}
+                >
+                    <div className={classes.toolbarIcon}>
+                        <IconButton onClick={this.handleClose}>
+                            <MenuOpenIcon />
+                        </IconButton>
+                    </div>
+                    <Divider />
+                   <SideBar />
+                </Drawer>
+               
                 {this.props.children}
             </div>
             
@@ -23,4 +113,4 @@ class AdminWrapper extends Component
     }
 }
 
-export default AdminWrapper;
+export default withStyles(styles)(AdminWrapper);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {Link as RouterLink} from 'react-router-dom';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -13,28 +14,38 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Dashboard from '@material-ui/icons/Dashboard';
 import Subject from '@material-ui/icons/Subject';
+import { Link } from '@material-ui/core';
+
+const ListItemLink = (props) => (
+    <ListItem button component={RouterLink} {...props} />
+);
+
 
 const SideBar = () => {
+
+   
     return(
         <List>
-            <ListItem button>
-                 <ListItemIcon>
-                     <Dashboard />
-                  </ListItemIcon>
-                 <ListItemText primary="Dashboard"></ListItemText>
-            </ListItem>
-             <ListItem button>
-                 <ListItemIcon>
-                     <Subject />
-                  </ListItemIcon>
-                 <ListItemText primary="Posts"></ListItemText>
-            </ListItem>
-             <ListItem button>
-                 <ListItemIcon>
-                     <AccountCircle />
-                  </ListItemIcon>
-                 <ListItemText primary="Users"></ListItemText>
-            </ListItem>
+            <ListItemLink to="/admin">
+                <ListItemIcon>
+                    <Dashboard />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard"></ListItemText>
+            </ListItemLink>
+        
+            <ListItemLink to="/admin/posts">
+                <ListItemIcon>
+                    <Subject />
+                </ListItemIcon>
+                <ListItemText primary="Posts"></ListItemText>
+            </ListItemLink>
+            
+            <ListItemLink  to="/admin/users">
+                <ListItemIcon>
+                    <AccountCircle />
+                </ListItemIcon>
+                <ListItemText primary="Users"></ListItemText>
+            </ListItemLink>
         </List>
     )
 }
